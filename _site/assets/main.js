a20b6809a3f43acdc3c53381a87ceedc447c8776
StoryWizard <51772314+StoryWizard@users.noreply.github.com>
2021-01-29 12:39:07 +1100

    Initial commit

---
+++ _site/assets/main.js
@@ -0,0 +1,93 @@
+
+
+$(document).ready(function() {
+
+	// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+	// This is the hamburger menu
+     $(".burger-button").click(function(){
+       // alert("bob");
+       $(".burger-button").toggleClass("active");
+       $(".burger-menu").toggleClass("active");
+     });
+	// end hamburger menu
+  $(".burger-menu").click(function(){
+    $(".burger-button").toggleClass("active");
+   $(".burger-menu").toggleClass("active");
+  });
+
+	// This is the pop-up gallery
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		}
+	});
+
+
+});
+
+
+// this is the accordian code
+
+$(document).ready(function() {
+
+	var allPanels = $('.accordion > dd').hide();
+
+	$('.accordion > dt > a').click(function() {
+		allPanels.slideUp();
+		$(this).parent().next().slideDown();
+		return false;
+	});
+
+
+});
+
+// end accordian
